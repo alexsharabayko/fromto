b7c6cde95bd74586768a17bac36e2ffab8cb389d
Alex Sharabayko <alexsharabayko@gmail.com>
2016-02-02 15:19:05 +0300

    Started develop server.

---
+++ server/index.js
@@ -0,0 +1,24 @@
+var express = require('express'),
+    app = express(),
+    port = 4000,
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser');
+
+mongoose.connect('localhost:27017/fromto');
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
+app.use(bodyParser.json());
+
+//app.use('/', require('./routes/user-route.js'));
+//app.use('/', require('./routes/product-route.js'));
+//app.use('/', require('./routes/dish-route.js'));
+//app.use('/', require('./routes/dish-property-route.js'));
+
+app.listen(port);
+console.log('Listening port ' + port);
