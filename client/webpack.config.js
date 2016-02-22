6f60923006fd7619e40412a1f2194091737f4e70
Alex Sharabayko <alexsharabayko@gmail.com>
2016-02-22 12:00:41 +0300

    Client firsts steps.

---
+++ client/webpack.config.js
@@ -0,0 +1,78 @@
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const webpack = require('webpack');
+
+module.exports = {
+    context: __dirname + '/frontend',
+
+    entry: {
+        app:  "./app"
+    },
+
+    output: {
+        path: __dirname + '/public',
+        publicPath: '/fromto/public/',
+        filename: "[name].js",
+        library: "[name]"
+    },
+
+    watch: NODE_ENV === 'development',
+
+    watchOptions: {
+        aggregateTimeout: 100
+    },
+
+    devtool: NODE_ENV === 'development' ? "cheap-inline-module-source-map" : null,
+
+    plugins: [
+        new webpack.NoErrorsPlugin(),
+        new webpack.DefinePlugin({
+            NODE_ENV: JSON.stringify(NODE_ENV),
+            LANG: JSON.stringify('ru')
+        })
+    ],
+
+    resolve: {
+        modulesDirectories: ['frontend', 'node_modules'],
+        extensions: ['', '.js', '.less']
+    },
+
+    resolveLoader: {
+        modulesDirectories: ['node_modules'],
+        moduleTemplates: ['*-loader', '*'],
+        extensions: ['', '.js']
+    },
+
+
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015', 'stage-0', 'react']
+                }
+            },
+            {
+                test: /\.(png|jpg|svg|ttf|eot|woff|woff2)$/,
+                loader: 'file?name=img/[name].[ext]?[hash]'
+            },
+            {
+                test: /\.less$/,
+                loaders: ['style', 'css', 'less']
+            }
+        ]
+    }
+};
+
+
+if (NODE_ENV == 'production') {
+    module.exports.plugins.push(
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false,
+                drop_console: true,
+                unsafe: true
+            }
+        })
+    );
+}
