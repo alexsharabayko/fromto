c4d92c59d70b3967ba285f7a19bc89d10f420355
Alex Sharabayko <alexsharabayko@gmail.com>
2016-03-21 12:58:45 +0300

    Registration

---
+++ client/frontend/core/file-upload/index.js
@@ -0,0 +1,38 @@
+import React, {PropTypes} from 'react';
+import BaseComponent from 'core/base-component';
+
+import './style.less';
+
+class FileUploadComponent extends BaseComponent {
+    constructor (props) {
+        super(props);
+
+        this.state = {
+            fileName: 'Upload'
+        };
+    }
+
+    handleFilesUpload (event) {
+        this.setState({
+            fileName: event.target.files[0].name
+        });
+
+        this.forceUpdate();
+
+        this.props.onSelect && this.props.onSelect(event.target.files);
+    }
+
+    render () {
+        var name = this.props.name || null;
+
+        return (
+            <div className="file-upload">
+                <span>{this.state.fileName}</span>
+                <input type="file" name={name}
+                       onChange={this.handleFilesUpload.bind(this)}/>
+            </div>
+        );
+    }
+}
+
+export default FileUploadComponent;
--- client/frontend/pages/register/index.js
+++ client/frontend/pages/register/index.js
@@ -4,12 +4,27 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as actions from 'actions/home-actions';
 import LogoComponent from 'core/logo';
+import FileUploadComponent from 'core/file-upload';
 
 import './style.less';
 
 class RegisterPage extends PageComponent {
+    constructor (props) {
+        super(props);
+
+        this.state = {};
+    }
+
     componentDidMount () {
         super.componentDidMount();
+
+        Array.from(this.refs.form.elements).forEach((element) => {
+            element.addEventListener('invalid', (event) => {
+                event.target.parentElement.classList.add('invalid');
+
+                event.target.setCustomValidity('');
+            });
+        });
     }
 
     render() {
@@ -21,15 +36,15 @@ class RegisterPage extends PageComponent {
                         <span className="register-logo_small">Registration</span>
                     </div>
 
-                    <form className="register-form">
+                    <form className="register-form" ref="form">
                         <div className="register-form_field">
-                            <input className="register-form_field_input" type="text"/>
+                            <input className="register-form_field_input" type="text" required minLength="10" maxLength="6"/>
                             <span className="register-form_field_label">Username</span>
                             <span className="register-form_field_error">Error validation message</span>
                             <span className="register-form_field_info">Success info</span>
                         </div>
 
-                        <div className="register-form_field">
+                        <div className="register-form_field valid">
                             <input className="register-form_field_input" type="password"/>
                             <span className="register-form_field_label">Password</span>
                             <span className="register-form_field_error">Error validation message</span>
@@ -43,7 +58,7 @@ class RegisterPage extends PageComponent {
                             <span className="register-form_field_info">Success info</span>
                         </div>
 
-                        <div className="register-form_field">
+                        <div className="register-form_field invalid">
                             <input className="register-form_field_input" type="text"/>
                             <span className="register-form_field_label">First name</span>
                             <span className="register-form_field_error">Error validation message</span>
@@ -65,12 +80,24 @@ class RegisterPage extends PageComponent {
                         </div>
 
                         <div className="register-form_field">
-                            <input className="register-form_field_input" type="radio"/>
-                            <input className="register-form_field_input" type="radio"/>
-                            <span className="register-form_field_label fixed">I'm a</span>
-                            <span className="register-form_field_error fixed">Error validation message</span>
-                            <span className="register-form_field_info fixed">Success info</span>
+                            <label className="register-form_field_radio">
+                                <input type="radio" name="go"/>
+                                <i className="fa fa-cab"></i>
+                                <span className="text">Driver</span>
+                            </label>
+                            <label className="register-form_field_radio">
+                                <input type="radio" name="go"/>
+                                <i className="fa fa-user"></i>
+                                <span className="text">Passenger</span>
+                            </label>
+                            <span className="register-form_field_label fixed">Register me as</span>
                         </div>
+
+                        <div className="register-form_field">
+                            <FileUploadComponent />
+                        </div>
+
+                        <button>Submit</button>
                     </form>
                 </div>
             </div>
